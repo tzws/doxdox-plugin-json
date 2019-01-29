70e46bcc3b73dc34adc9d5d9c423cd1de4ed1a2e
chunfei.zhang <Chunfei.Zhang@thomsonreuters.com>
2019-01-29 07:41:06 +0000

    Add files

---
+++ README.md
@@ -0,0 +1,16 @@
+# doxdox-plugin-json
+
+> Json plugin for doxdox.
+
+
+## Install
+
+```bash
+$ npm install doxdox doxdox-plugin-json --save-dev
+```
+
+## Usage
+
+```bash
+$ doxdox 'src/**/*.js' --layout json --output somejson.js
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Json plugin for doxdox.
+ *
+ * @example parseInputs(inputs, {'parser': 'dox', 'layout': 'json'}).then(content => console.log(content));
+ * @param {Array} data Methods parsed using a doxdox parser.
+ * @return {Promise} Promise with generated content.
+ * @public
+ */
+
+const plugin = data => new Promise((resolve, reject) => {
+
+  var result = {};
+  data.files.filter(item => item.methods.length > 0).map(everyEach => 
+    {
+      everyEach.methods.map(m1 => {
+        result[m1.name] = m1.tags.property.map(elem => elem.name).join('');
+      });
+    }
+  );
+
+  return resolve((JSON.stringify(result, null, 2)));
+});
+
+module.exports = plugin;
